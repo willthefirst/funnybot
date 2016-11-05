aa49f56413d1ff70a0ac9b61f2c678382f5198c4
Will Moritz <will@crowdfavorite.com>
2016-11-05 16:11:53 -0400

    working random suggestions for location, emotion, and anything

--- bot.js
+++ bot.js
@@ -1,3 +1,23 @@
 const botBuilder = require('claudia-bot-builder');
+const fbTemplate = require('claudia-bot-builder').fbTemplate;
+const suggestions = require('./suggestions');
+console.log(suggestions.getRandom('NON_GEO_LOCATION'));
 
-module.exports = botBuilder(request => `Hello from space explorer bot! Your request was: ${request.text}`);
+module.exports = botBuilder(request => {
+  console.log(`BOOM: Request type: ${request.postback}`);
+
+  // Introduce ourself
+  if(!request.postback) {
+    return [
+      `Oh hey. I can yell out improv suggestions.`,
+      new fbTemplate.Button('Whadda ya need?')
+      .addButton('A non-geographical location', 'NON_GEO_LOCATION')
+      .addButton('An emotion', 'EMOTION')
+      .addButton('Anything at all', 'ANYTHING')
+      .get()
+    ]
+  }
+
+  // Reply with a suggestion
+  return suggestions.getRandom(request.text)
+});
